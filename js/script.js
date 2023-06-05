779e45abbcfea2cbf1015c73be733e6b3cc3d9d2
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-05 13:30:54 +0200

    add for logic and print on page

--- js/script.js
+++ js/script.js
@@ -14,3 +14,25 @@ const products1 = ['latte', 'uova', 'pane'];
 const products2 = ['spaghetti', 'pomodoro', 'parmiggiano'];
 
 // fine operazioni preliminari
+
+///--------------------------
+
+// logica for
+
+// creo una variabile d'appoggio
+
+let listproducts1 = '';
+
+// creo un ciclo
+
+for(let i = 0; i < products1.length; i++){
+    // monto la lista all'interno dell'elemento
+    listproducts1 += ' ' + products1[i];
+};
+//stampo la lista
+forlistElement.innerText = listproducts1;
+
+///---------------------------------------------------
+
+// logica while
+
