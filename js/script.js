5b95eebe481f789369fd2a5bfa4568011b2a2a35
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-05 13:24:55 +0200

    add pre operetion

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 //check console log
 console.log('ok js');
 
+// ## Operazioni preliminari
+
+//recupero elementi dal DOM
+
+const forlistElement = document.getElementById('market-list-1');
+const whilelistElement = document.getElementById('market-list-2');
+
+// creo due Array conteneti una lista della spesa
+
+const products1 = ['latte', 'uova', 'pane'];
+const products2 = ['spaghetti', 'pomodoro', 'parmiggiano'];
+
+// fine operazioni preliminari
